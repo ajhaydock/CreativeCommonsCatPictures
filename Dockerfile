f0ac7361752d1f51cf75af038bf8cf0b13271dd1
Alex Haydock <alex@alexhaydock.co.uk>
2020-01-04 21:32:57 +0000

    Make Docker container work better for armv7l

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
-FROM registry.gitlab.com/alexhaydock/dockerfiles:jekyll as builder
+FROM debian as builder
 LABEL maintainer "Alex Haydock <alex@alexhaydock.co.uk>"
 
+# Install Jekyll deps
+RUN apt-get update && apt-get install -y bundler ruby-dev zlib1g-dev
+
+# Copy site content into container
 COPY . /tmp/darkwebkittens.xyz
 WORKDIR /tmp/darkwebkittens.xyz
+
+# Install the relevant gems with Bundler and then build the site
 RUN bundle install
 RUN bundle exec jekyll build
 
--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -3,9 +3,11 @@ set -e
 if [ "$EUID" -eq 0 ]; then echo "Please do not run as root. Please add yourself to the 'docker' group."; exit; fi
 
 if [ "$(uname -m | grep 'x86_64' | wc -l)" -gt 0 ]; then
+    echo "This looks like an x86_64 system. Building for that platform."
     docker build --no-cache -t registry.gitlab.com/alexhaydock/darkwebkittens.xyz .
     docker push registry.gitlab.com/alexhaydock/darkwebkittens.xyz
 elif [ "$(uname -m | grep 'armv7l' | wc -l)" -gt 0 ]; then
+    echo "This looks like an armv7l system. Building for that platform."
     docker build --no-cache -t registry.gitlab.com/alexhaydock/darkwebkittens.xyz:armv7l .
     docker push registry.gitlab.com/alexhaydock/darkwebkittens.xyz:armv7l
 else
