7cc7ac9e46ed22e427b95a0817f7ada9711f8e96
Alex Haydock <alex@alexhaydock.co.uk>
2020-01-04 19:51:02 +0000

    Add Pi build

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-set -xe
+set -e
 if [ "$EUID" -eq 0 ]; then echo "Please do not run as root. Please add yourself to the 'docker' group."; exit; fi
 
-docker build --no-cache -t registry.gitlab.com/alexhaydock/darkwebkittens.xyz .
-docker push registry.gitlab.com/alexhaydock/darkwebkittens.xyz
+if [ "$(uname -m | grep 'x86_64' | wc -l)" -gt 0 ]; then
+    docker build --no-cache -t registry.gitlab.com/alexhaydock/darkwebkittens.xyz .
+    docker push registry.gitlab.com/alexhaydock/darkwebkittens.xyz
+elif [ "$(uname -m | grep 'armv7l' | wc -l)" -gt 0 ]; then
+    docker build --no-cache -t registry.gitlab.com/alexhaydock/darkwebkittens.xyz:armv7l .
+    docker push registry.gitlab.com/alexhaydock/darkwebkittens.xyz:armv7l
+else
+    echo "Sorry, I don't understand this processor architecture."
+    echo "Please build manually."
+fi
